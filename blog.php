d64acc9cc9c2f6d26cee6b632de907710141c12b
Kuldeep Gautam <cs20mtech01004@iith.ac.in>
2020-05-02 14:08:44 +0530

    Added support for forms

--- blog.php
+++ blog.php
@@ -1,8 +1,8 @@
-<?php
+<!--<?php
 if($_POST["message"]) {
     mail("kuldeep.gautam075@gmail.com", "Form to email message", $_POST["message"], "From: an@email.address");
 }
-?>
+?>-->
 <!DOCTYPE html>
 <html lang="zxx" class="no-js">
 
